d46a6fc2bc21b3009b7d875b8080376e1ab359e7
yeonann <lkim82@naver.com>
2023-03-14 11:48:22 +0900

    main2, main3, increase 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -7,10 +7,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    private int count;
+
+    public HomeController() {
+        this.count = -1;
+    }
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain () {
         return "안녕하세요!!!";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2 () {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3 () {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease () {
+        count++;
+        return count;
     }
 
 }
